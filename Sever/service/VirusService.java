467b3a642ea38eef07afb8413484e918bae4efab
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-05-12 11:20:51 +0900

    Create VirusService.java

---
+++ Sever/service/VirusService.java
@@ -0,0 +1,22 @@
+package com.example.virus_information.service;
+
+import com.example.virus_information.entity.Virus;
+import com.example.virus_information.repository.VirusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VirusService {
+    private final VirusRepository virusRepository;
+
+    @Autowired
+    public VirusService(VirusRepository virusRepository) {
+        this.virusRepository = virusRepository;
+    }
+
+    public List<Virus> getAllViruses() {
+        return virusRepository.findAll();
+    }
+}
