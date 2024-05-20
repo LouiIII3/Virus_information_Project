e68841a0866c6f9981cafd6561b60fcbb79a8962
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-05-20 10:18:16 +0900

    Create RouteService.java

---
+++ Sever/service/RouteService.java
@@ -0,0 +1,25 @@
+package com.example.virus_information.service;
+
+import com.example.virus_information.dto.RouteDTO;
+import com.example.virus_information.entity.Route;
+import com.example.virus_information.repository.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RouteService {
+
+    private final RouteRepository routeRepository;
+
+    @Autowired
+    public RouteService(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
+    }
+
+    public List<Route> findByIdentifier(Long identifier) {
+        return routeRepository.findByIdentifier(identifier);
+    }
+}
